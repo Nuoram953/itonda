f9f4254ff32381ad1a6527242a2020d32bc89203
Nuoram953 <antoinemaroun@gmail.com>
2026-07-13 18:21:15 -0400

    feat(web): add basic scaffolding
    Added tailwind config, react base code with vite

---
+++ apps/web/src/main.tsx
@@ -0,0 +1,66 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "../input.css";
+
+import {
+  createRootRoute,
+  createRouter,
+  createRoute,
+  RouterProvider,
+  Link,
+  Outlet,
+} from "@tanstack/react-router";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Home } from "./home";
+
+const rootRoute = createRootRoute({
+  component: () => (
+    <>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/library">Library</Link>
+      </nav>
+
+      <Outlet />
+    </>
+  ),
+
+  notFoundComponent: () => (
+    <div>
+      <p>Page not found</p>
+      <Link to="/">Start Over</Link>
+    </div>
+  ),
+});
+
+const indexRoute = createRoute({
+  getParentRoute: () => rootRoute,
+  path: "/",
+  component: () => <Home />,
+});
+
+const libraryRoute = createRoute({
+  getParentRoute: () => rootRoute,
+  path: "library",
+  component: () => <h1>Library</h1>,
+});
+
+const routeTree = rootRoute.addChildren([indexRoute, libraryRoute]);
+
+const router = createRouter({
+  routeTree,
+  defaultPreload: "intent",
+  defaultPreloadStaleTime: 0,
+  defaultStaleTime: 5000,
+  scrollRestoration: true,
+});
+
+const queryClient = new QueryClient();
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
+  </StrictMode>,
+);
