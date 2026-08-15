7f3145e0f4833cd15eba73d63690a2d7262d9803
Nuoram953 <antoinemaroun@gmail.com>
2026-08-14 22:42:16 -0400

    feat(server): add middleware for logging http call

--- apps/server/src/api/mod.rs
+++ apps/server/src/api/mod.rs
@@ -3,6 +3,7 @@ pub mod assets;
 pub mod error;
 pub mod extractor;
 pub mod media;
+pub mod middleware;
 pub mod openapi;
 pub mod response;
 
@@ -11,7 +12,11 @@ use axum::Router;
 use crate::state::AppState;
 
 pub fn router() -> Router<AppState> {
-    Router::new().merge(
-        media::routes::router().merge(assets::routes::router().merge(agents::routes::router())),
-    )
+    Router::new()
+        .merge(media::routes::router())
+        .merge(assets::routes::router())
+        .merge(agents::routes::router())
+        .layer(axum::middleware::from_fn(
+            middleware::api_logging_middleware,
+        ))
 }
